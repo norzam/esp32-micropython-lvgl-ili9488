6ea00309c1f7166a5035d474483fd6b869e889f1
norzam <norzam@gmail.com>
2020-09-28 13:37:47 +0800

    first

---
+++ trigger.py
@@ -0,0 +1,19 @@
+class Trigger:
+
+    self.name = ""
+
+    self.bhour      = 0
+    self.bminute    = 0
+    self.bsecond    = 0
+
+    self.ebhour     = 0
+    self.eminute    = 0
+    self.esecond    = 0
+
+    self.hourdur    = 0
+    self.minutedur  = 0
+    self.seconddur  = 0
+
+    self.isArmed    = False
+    self.isRansensor= False
+    
