0c16d894b35df2f2a52690007bafbf9316a522ce
norzam <norzam@gmail.com>
2020-09-28 15:56:05 +0800

    trigger class

--- trigger.py
+++ trigger.py
@@ -1,19 +1,35 @@
 class Trigger:
-
-    self.name = ""
-
-    self.bhour      = 0
-    self.bminute    = 0
-    self.bsecond    = 0
-
-    self.ebhour     = 0
-    self.eminute    = 0
-    self.esecond    = 0
-
-    self.hourdur    = 0
-    self.minutedur  = 0
-    self.seconddur  = 0
-
-    self.isArmed    = False
-    self.isRansensor= False
     
+    def __init__(self):
+
+        self.name       = ""
+        self.description= ""
+
+        self.bhour      = 0
+        self.bminute    = 0
+        self.bsecond    = 0
+
+        self.ebhour     = 0
+        self.eminute    = 0
+        self.esecond    = 0
+
+        self.hourdur    = 0
+        self.minutedur  = 0
+        self.seconddur  = 0
+
+        self.dayOfWeek= [True, True, True, True, True, True, True, True]
+    
+        self.usingCalendar  = False
+    
+        self.usingFlowSensor= False
+        self.allowFlowSensorOveride = False
+        self.FlowSensorMin  = 5
+        self.FlowSensorMax  = 100
+
+        self.isTriggered    = False
+        self.isArmed        = False
+        self.isRansensor    = False
+        self.isUsingMETAPI  = False
+       
+
+
